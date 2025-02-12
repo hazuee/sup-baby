51c65dda60f554246c81807704e9321b41bdb827
hazuee <theblockmob@gmail.com>
2025-02-12 18:14:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
     // Your Valentine's name that will appear in the title
-    // Example: "Jade", "Sarah", "Mike"
+    // Example: "Jade", "Melrose"
     valentineName: "Melrose",
 
     // The title that appears in the browser tab
